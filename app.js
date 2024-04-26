f25d6db77488621c0ef5bc57809f5041e7734a30
Kenneth V <103289124+kennethkietvuong@users.noreply.github.com>
2024-04-26 00:50:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const addTaskButton = document.getElementById("add-task");
 
 // Function that loads JSON file for webpage to display task list
 function loadTasks() {
-    fetch('task-list/hard-coded-tasks.json')
+    fetch('task-list/hard-coded-tasks.JSON')
         .then(response => response.json())
         .then(data => {
             data.forEach(task => {
@@ -231,48 +231,48 @@ function randomNumberGenerator () {
 
 loadTasks();
 
-// Function to fetch and display tasks from JSON
-function displayTasks() {
-    fetch('task-list/hard-coded-tasks.json')
-        .then(response => response.json())
-        .then(tasks => {
-            const taskWidget = document.getElementById('taskWidget');
-            tasks.forEach(task => {
-                const taskDiv = document.createElement('div');
-                taskDiv.classList.add('task');
+// // Function to fetch and display tasks from JSON
+// function displayTasks() {
+//     fetch('task-list/hard-coded-tasks.json')
+//         .then(response => response.json())
+//         .then(tasks => {
+//             const taskWidget = document.getElementById('taskWidget');
+//             tasks.forEach(task => {
+//                 const taskDiv = document.createElement('div');
+//                 taskDiv.classList.add('task');
 
-                taskDiv.innerHTML = `
-                    <div class="task-id">Task ID: ${task.id}</div>
-                    <div>Task: ${task.task}</div>
-                    <div class="task-completed">Completed: ${task.completed ? 'Yes' : 'No'}</div>
-                    <div class="task-priority">Priority: ${task.priority}</div>
-                    <div>Due: ${task.due}</div>
-                `;
-                taskWidget.appendChild(taskDiv);
-            });
-        })
-        .catch(error => console.error('Error fetching tasks:', error));
-}
+//                 taskDiv.innerHTML = `
+//                     <div class="task-id">Task ID: ${task.id}</div>
+//                     <div>Task: ${task.task}</div>
+//                     <div class="task-completed">Completed: ${task.completed ? 'Yes' : 'No'}</div>
+//                     <div class="task-priority">Priority: ${task.priority}</div>
+//                     <div>Due: ${task.due}</div>
+//                 `;
+//                 taskWidget.appendChild(taskDiv);
+//             });
+//         })
+//         .catch(error => console.error('Error fetching tasks:', error));
+// }
 
-function getRandomImageUrl() {
-    var numPokemon = 80;
-    var randomIndex = Math.floor(Math.random() * numPokemon)+1;
-    var randomNumberString = randomIndex.toString();
-    while (randomNumberString.length < 3) {
-        randomNumberString = '0' + randomNumberString;
-    }
-    return "UI_src/imgs/pokemon/pokemon_icon_" + randomNumberString + "_00.png";
-}
+// function getRandomImageUrl() {
+//     var numPokemon = 80;
+//     var randomIndex = Math.floor(Math.random() * numPokemon)+1;
+//     var randomNumberString = randomIndex.toString();
+//     while (randomNumberString.length < 3) {
+//         randomNumberString = '0' + randomNumberString;
+//     }
+//     return "UI_src/imgs/pokemon/pokemon_icon_" + randomNumberString + "_00.png";
+// }
 
-function displayRandomImage() {
-    for (var i = 1; i < 5; i++) {
-        var randomImageUrl = getRandomImageUrl();
-        var imageElement = document.getElementById("random-image" + i);
-        imageElement.src = randomImageUrl;
-        imageElement.alt = randomImageUrl;
-    }
-}
+// function displayRandomImage() {
+//     for (var i = 1; i < 5; i++) {
+//         var randomImageUrl = getRandomImageUrl();
+//         var imageElement = document.getElementById("random-image" + i);
+//         imageElement.src = randomImageUrl;
+//         imageElement.alt = randomImageUrl;
+//     }
+// }
 
-// Call the function to display tasks when the page loads
-displayTasks();
-displayRandomImage()
+// // Call the function to display tasks when the page loads
+// displayTasks();
+// displayRandomImage()
